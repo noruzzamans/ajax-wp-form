82fad5f575f54c30fe24ddafa9867cf6ee4edbbe
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-06-01 12:41:57 +0600

    fix data insert issues in database

--- includes/class-wp-form-ajax.php
+++ includes/class-wp-form-ajax.php
@@ -1,64 +1,61 @@
 <?php
 
 class WP_Form_Ajax {
-    public function wp_form_submit( $args = [] ) {
+    public function wp_form_submit() {
+
+        $nonce = $_POST['nonce'];
 
         if ( ! wp_verify_nonce( $nonce, 'wp_form_nonce' ) ) {
             die( 'Busted!' );
         }
 
-        $fname = isset( $_POST['fname'] ) ? sanitize_text_field( $_POST['fname']) : '';
-        $lname = isset( $_POST['lname'] ) ? sanitize_text_field( $_POST['lname']) : '';
-        $email = isset( $_POST['email'] ) ? sanitize_email( $_POST['email']) : '';
-        $subject = isset( $_POST['subject'] ) ? sanitize_text_field( $_POST['subject']) : '';
-        $message = isset( $_POST['message'] ) ? sanitize_textarea_field( $_POST['message']) : '';
+        $data    = $_POST['data'];
+        $fname   = isset( $data['fname'] ) ? sanitize_text_field( $data['fname'] ) : '';
+        $lname   = isset( $data['lname'] ) ? sanitize_text_field( $data['lname'] ) : '';
+        $email   = isset( $data['email'] ) ? sanitize_email( $data['email'] ) : '';
+        $subject = isset( $data['subject'] ) ? sanitize_text_field( $data['subject'] ) : '';
+        $message = isset( $data['message'] ) ? sanitize_textarea_field( $data['message'] ) : '';
 
-
-
-        global $wpdb;
-
-        if ( empty( $_POST['fname'] ) ) {
+        if ( empty( $data['fname'] ) ) {
             return new \WP_Error( 'fname_error', 'First Name is required' );
         }
 
-        if ( empty( $_POST['lname'] ) ) {
+        if ( empty( $data['lname'] ) ) {
             return new \WP_Error( 'lname_error', 'Last Name is required' );
         }
 
-        if ( empty( $_POST['email'] ) ) {
+        if ( empty( $data['email'] ) ) {
             return new \WP_Error( 'email_error', 'Email is required' );
         }
 
-        if ( empty( $_POST['subject'] ) ) {
+        if ( empty( $data['subject'] ) ) {
             return new \WP_Error( 'subject_error', 'Subject is required' );
         }
 
-        if ( empty( $_POST['message'] ) ) {
+        if ( empty( $data['message'] ) ) {
             return new \WP_Error( 'message_error', 'Message is required' );
         }
 
-        $defaults = [
-            'fname'      => '',
-            'lname'      => '',
-            'email'      => '',
-            'subject'    => '',
-            'message'    => '',
-            'created_at' => current_time( 'mysql' ),
-            'created_by' => get_current_blog_id(),
-        ];
-
-        $data     = wp_parse_args( $args, $defaults );
+        global $wpdb;
         $inserted = $wpdb->insert(
             "{$wpdb->prefix}wp_form",
-            $data,
+            [
+                'fname'      => $fname,
+                'lname'      => $lname,
+                'email'      => $email,
+                'subject'    => $subject,
+                'message'    => $message,
+                'created_by' => get_current_blog_id(),
+                'created_at' => current_time( 'mysql' ),
+            ],
             [
                 '%s',
                 '%s',
                 '%s',
                 '%s',
                 '%s',
-                '%s',
                 '%d',
+                '%s',
             ]
         );
 
@@ -66,9 +63,11 @@ class WP_Form_Ajax {
             return new \WP_Error( 'There was an error saving the form' );
         }
 
-        return $wpdb->insert_id;
+        wp_send_json_success([
+            'message' => 'Form submitted successfully',
+        ]);
 
-        exit;
+        return $wpdb->insert_id;
     }
 
 }
--- public/js/wp-form-public.js
+++ public/js/wp-form-public.js
@@ -5,14 +5,27 @@
 		$('#wp_custom_form form').on('submit', function (e) {
 			e.preventDefault();
 
-			var data = $(this).serialize();
+			// var data = $(this).serialize();
+			
+			var fname = $(this).find('input[name="fname"]').val();
+			var lname = $(this).find('input[name="lname"]').val();
+			var email = $(this).find('input[name="email"]').val();
+			var subject = $(this).find('input[name="subject"]').val();
+			var message = $(this).find('textarea[name="message"]').val();
+
 
 			$.ajax({
 				url: wp_form.ajaxurl,
 				type: 'POST',
 				data: {
 					action: wp_form.action,
-					data: data,
+					data: {
+						fname: fname,
+						lname: lname,
+						email: email,
+						subject: subject,
+						message: message
+					},
 					nonce: wp_form.nonce,
 
 				}
